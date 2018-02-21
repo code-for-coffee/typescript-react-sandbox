74abde42dbe601dd01673e3e8d4beecaf2c54dfa
James Traver <jtraver@gogoair.com>
2018-02-20 22:24:59 -0600

    adding ts validators kevin suggested

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,16 +1,34 @@
 import * as React from "react";
+import {AccountModel} from '../models/AccountModel';
+import { on } from "cluster";
 
 export interface HelloProps { 
-    compiler: string; 
-    framework: string; 
+  compiler: string; 
+  framework: string; 
 }
+
+let kevin = new AccountModel();
+kevin.age = 30;
+kevin.dateCreated = new Date();
+kevin.email = 'kevin@typescript.js'; // break me! add another .
+kevin.username = 'gobulls';
+
+const onSuccess = () => {
+  console.log('You win!');
+};
+
+const onError = () => {
+  console.log('You lose!');
+};
+
+AccountModel.validate(kevin, onSuccess, onError);
 
 // stateless
 // export const Hello = (props: HelloProps) => 
 //     <h1>Hello from {props.compiler} and {props.framework}!</h1>;
 
 export class Hello extends React.Component<HelloProps, {}> {
-    render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
-    }
+  render() {
+    return <h1>Hi Kevin! Check out {this.props.compiler} and {this.props.framework}!</h1>;
+  }
 }
---
+++ src/models/AccountModel.tsx
@@ -0,0 +1,25 @@
+import {validate, Contains, IsInt, Length, IsEmail, IsDate} from "class-validator";
+
+export class AccountModel {
+  @Length(10,22)
+  username: string;
+
+  @IsInt()
+  age: number;
+
+  @IsEmail()
+  email: string;
+
+  @IsDate()
+  dateCreated: Date;
+
+  static validate(account: AccountModel, onSuccess: any, onFail: any) {
+    validate(account).then(errors => {
+      if (errors.length > 0) {
+        onSuccess();
+      } else {
+        onFail()
+      }
+    })
+  }
+};
